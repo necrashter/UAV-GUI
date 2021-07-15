1ca11a4080ab4bd0732ebfdd464a77143084b32b
Ertuğrul Tiyek <ertugrultyk@gmail.com>
2021-07-16 00:16:01 +0300

    Stable bytes

--- js_adapter/scripts/script.js
+++ js_adapter/scripts/script.js
@@ -1,3 +1,5 @@
+var veri;
+
 document.addEventListener('DOMContentLoaded', () =>{
 
 	const baglanTusu = document.getElementById("baglanTusu");
@@ -13,57 +15,58 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 		await port.open({
 			"baudRate" : "9600",
-			"bufferSize":"1800"
+			"bufferSize":"10240"
 		});
-		 //port.bufferSize = 53
 
-		const textDecoder = new TextDecoderStream();
-		const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
-		const reader = textDecoder.readable.getReader();
+		const reader = port.readable.getReader();
 
-		var splitData;
 		let utf8Encode = new TextEncoder();
 
 
-//-------------------------- do it in the loop function ----------
 		while (true) {
-		  var { value, done } = await reader.read();
+		  const { value, done } = await reader.read();
 		  if (done) {
-		    // Allow the serial port to be closed later.
+
 		    reader.releaseLock();
 		    break;
 		  }
 
-		  // value is a Uint8Array.
-			//value = utf8Encode.encode(value);
-			//console.log(value);
+			if(value.length === 32)
 
-			if(value.includes("\n")){
-				splitData = value.split("\n");
+				data = value;
+			else if(value.length === 20){
 
-				data += splitData[0];
+				data = new Uint8Array([data[0], data[1], data[2], data[3],
+					data[4], data[5], data[6], data[7],
+					data[8], data[9], data[10], data[11],
+					data[12], data[13], data[14], data[15],
+					data[16], data[17], data[18], data[19],
+					data[20], data[21], data[22], data[23],
+					data[24], data[25], data[26], data[27],
+					data[28], data[29], data[30], data[31],
+					value[0], value[1], value[2], value[3],
+					value[4], value[5], value[6], value[7],
+					value[8], value[9], value[10], value[11],
+					value[12], value[13], value[14], value[15],
+					value[16], value[17], value[18], value[19],
+				]);
+			}else {
+				//console.error("err");
+			}
 
-				for (var i = 0; i < data.length; i+=4){
-					bData = [data[i+3], data[i+2], data[i+1], data[i]];
-					//bData.reverse
-					var buf = new ArrayBuffer(4);
-					// Create a data view of it
-					var view = new DataView(buf);
-					console.log(view);
+			for (var i = 0; i < data.length; i+=4){
 
-					//set bytes
-					bData.forEach(function (b, i) {
-					    view.setUint8(i, b);
-					});
+				var bData = new Uint8Array([data[i+3], data[i+2], data[i+1], data[i]]);
 
-					// Read the bits as a float; note that by doing this, we're implicitly
-					// converting it from a 32-bit float into JavaScript's native 64-bit double
-					// var num = view.getFloat32(0);
-					// console.log(num);
-				}
+				var buf = new ArrayBuffer(4);
+				var view = new DataView(buf);
 
-				data = splitData[1];
-			}//endIf
+				bData.forEach(function (b, i) {
+				    view.setUint8(i, b);
+				});
+				var num = view.getFloat32(0);
+				console.log(num);
+			}
 		}//endWhile
 
 		//----------------------------------------------------------------
